e2cd1a4dabbc5c0d23988c58a96c8c29d9235650
Francois <f.gueguen@24market.fr>
2026-01-08 17:01:29 +0900

    chore: scaffold project config

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+const js = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    ignores: ["dist/**", "node_modules/**", "eslint.config.js"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "no-undef": "off",
+    },
+  },
+];
